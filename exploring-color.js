89eb9f0a35adfeec5b050c482b5d3ddfca5305a2
J.D. Zamfirescu-Pereira <jd@zamfi.net>
2014-04-24 12:30:40 -0700

    cleaner?

--- exploring-color.js
+++ exploring-color.js
@@ -1,3 +1,4 @@
 $(function() {
-  $('pre').wrap('<div class="wrapped-code hidden"><span class="show-code-link">Show code &raquo;</span><span class="hide-code-link">&times;</span></div>');
+  $('pre').wrap('<div class="wrapped-code hidden"></div>');
+  $('.wrapped-code').prepend('<span class="show-code-link">Show code &raquo;</span><span class="hide-code-link">&times;</span>');
 })
